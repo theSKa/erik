e506e012f4b83674363e6673e26b854d648a1177
Petru Dimulescu <petru.dimulescu@transferwise.com>
2016-04-25 12:59:42 +0100

    Add parroting

--- erik/reactions/general.rb
+++ erik/reactions/general.rb
@@ -1,26 +1,13 @@
 module Erik
   module Commands
     class General < SlackRubyBot::Commands::Base
-      match /(W|w)hen(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "After I drink my proteins.")
-      end
-      match /(W|w)here(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "At the gym.")
-      end
-      match /(H|h)ow(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "By squatting. :troll:")
-      end
-      match /(W|w)hat(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "Only squats. Maybe deadlifts sometimes.")
-      end
-      match /(W|w)ith(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "With what? No way dude, we are dead serious about squats.")
-      end
-      match /(\w*)analysis(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "Yup. That is me. *Analysis* is my *middle name*.\nAnd *Squats*.")
-      end
-      match /(\w*)(Erik|erik|@erik|key|api)(\s.*)?/ do |client, data, match|
-        client.say(channel: data.channel, text: "Give me your API key!")
+      command 'say' do |client, data, match|
+        submatch = /say \<#(?<channel>[\w-]*)\> (?<message>.*)/.match(data.text)
+        if submatch['channel'] && submatch['message']
+          client.say(channel: submatch['channel'], text: submatch['message'])
+        else
+          client.say(channel: data.channel, text: "?")
+        end
       end
     end
   end
